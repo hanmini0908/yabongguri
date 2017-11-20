a95ccdf00468bea0b6c480e1db2e94a058bffc0d
iambns2 <iambns2@nate.com>
2017-11-20 12:22:36 +0900

    루크 레이드 성공 Activity에서 홈으로 간 뒤 앱 종료하면 성공 activity로 돌아가는 현상 수정

--- DnfLukeLaidMap/app/src/main/java/com/yabongguri/dnflukelaidmap/MainActivity.java
+++ DnfLukeLaidMap/app/src/main/java/com/yabongguri/dnflukelaidmap/MainActivity.java
@@ -5,9 +5,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -48,7 +48,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 intent = new Intent(this, HellChannel.class);
                 break;
             case R.id.btn_close:
-                finish();
+                finishApp();
                 break;
         }
 
@@ -64,7 +64,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         alertdialog.setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                finish();
+                finishApp();
             }
         });
 
@@ -78,5 +78,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         AlertDialog alert = alertdialog.create();
         alert.setTitle(getString(R.string.exit));
         alert.show();
+    }
+
+    private void finishApp() {
+        ActivityCompat.finishAffinity(this);
     }
 }
